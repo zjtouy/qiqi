52a12a5900b77ea5855f185b3812665388d29444
zjtouy <3140813253@qq.com>
2021-10-19 00:06:02 +0800

    create ParkingSystem class

---
+++ ParkingSystem.java
@@ -0,0 +1,11 @@
+public class ParkingSystem implements IParking{
+...
+public static void main(String[] args) throws Exception {
+        IParams params = IParking.parse();//ParkingSystem.parse();
+        ParkingSystem ps = new ParkingSystem(params.getBig(), params.getMedium(),params.getSmall());
+        ArrayList<Integer> plan = params.getPlanParking();
+        for (int i = 0; i < plan.size(); i++) {
+            ps.addCar(plan.get(i));
+        }
+        ps.print();
+    }
